3b3f4a803eb14a398f45971866ed9855a4c22154
Mespi <igomisz@gmail.com>
2020-10-13 17:46:19 +0200

    Websocket fix?

--- vue.config.js
+++ vue.config.js
@@ -28,6 +28,7 @@ module.exports = {
                 changeOrigin: true
             }
         },
-        disableHostCheck: true
+        disableHostCheck: true,
+        transportMode: 'ws'
     }
 };
